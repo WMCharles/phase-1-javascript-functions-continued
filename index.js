87bae07496249061abfe1436e75eb47f98287ad7
WMCharles <charles.wafula@student.moringaschool.com>
2022-08-22 19:01:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+function wrapAdjective(adj="*"){
+    const inner = function (adjx = "special"){
+        return `You are ${adj}${adjx}${adj}!`;
+    }
+    return inner;
+}
+
+console.log(wrapAdjective("|")('clever'))
